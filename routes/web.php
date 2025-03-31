c3dc7f63a448cd37f3ade9f621bf4afbaace3eee
arabkhondev <starc9899@gmail.com>
2025-03-31 15:55:35 +0500

    web page add resoues routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,7 @@ Route::get('/dashboard', function () {
 
 Route::resources([
     'posts'=> PostController::class,
+    'users'=>UserController::class
 ]);
 
 require __DIR__.'/auth.php';
