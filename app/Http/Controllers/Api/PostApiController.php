854f55ab546f92c6bb52f3a119469917d8d104a5
arabkhondev <starc9899@gmail.com>
2025-03-31 22:45:05 +0500

    integration with swagger

--- app/Http/Controllers/Api/PostApiController.php
+++ app/Http/Controllers/Api/PostApiController.php
@@ -9,11 +9,60 @@ use Illuminate\Http\Request;
 
 class PostApiController extends Controller
 {
+     /**
+     * @OA\Get(
+     *      path="/api/posts",
+     *      operationId="getPostList",
+     *      tags={"Posts"},
+     *      summary="Barcha postlarni olish",
+     *      description="Postlar ro‘yxatini qaytaradi",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Muvaffaqiyatli",
+     *          @OA\JsonContent(
+     *              type="array",
+     *              @OA\Items(
+     *                  type="object",
+     *                  @OA\Property(property="id", type="integer", example=1),
+     *                  @OA\Property(property="title", type="string", example="Post sarlavhasi"),
+     *                  @OA\Property(property="body", type="string", example="Post matni"),
+     *              )
+     *          )
+     *      )
+     * )
+     */
     public function index()
     {
         $posts = Post::orderBy("created_at","desc")->paginate(10);
         return response()->json($posts);
     }
+
+    /**
+ * @OA\Post(
+ *      path="/api/posts",
+ *      operationId="createPost",
+ *      tags={"Posts"},
+ *      summary="Yangi post yaratish",
+ *      description="Post yaratish API",
+ *      @OA\RequestBody(
+ *          required=true,
+ *          @OA\JsonContent(
+ *              required={"title","body"},
+ *              @OA\Property(property="title", type="string", example="Yangi post"),
+ *              @OA\Property(property="body", type="string", example="Postning batafsil ma'lumoti"),
+ *          )
+ *      ),
+ *      @OA\Response(
+ *          response=201,
+ *          description="Post yaratildi",
+ *          @OA\JsonContent(
+ *              @OA\Property(property="id", type="integer", example=1),
+ *              @OA\Property(property="title", type="string", example="Yangi post"),
+ *              @OA\Property(property="body", type="string", example="Postning batafsil ma'lumoti"),
+ *          )
+ *      )
+ * )
+ */
 
     public function store(StorePostRequest $request)
     {
