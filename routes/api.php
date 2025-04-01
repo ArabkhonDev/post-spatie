e51639f2043cb684d6995028eb5d04a01b512efd
arabkhondev <starc9899@gmail.com>
2025-04-01 17:23:40 +0500

    change api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\PostApiController;
+use App\Http\Controllers\Api\UserApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth');
+
+
+Route::apiResources([
+    'posts'=>PostApiController::class,
+    'users'=>UserApiController::class,
+]);
